00bbdd5e2cf4e453bbfee96fc13c53d6ecec47b6
Rufus Raghunath <rufusraghunath@gmail.com>
2016-01-31 17:51:19 +0000

    completed challenge; .injekt now works

--- lib/injekt.rb
+++ lib/injekt.rb
@@ -1,7 +1,8 @@
 class Array
 
-  def injekt array, memo=0
+  def injekt array, memo=array[0]
     array.each do |i|
+      next if i == array[0]
       memo = yield memo, i
     end
     memo
--- spec/injekt_spec.rb
+++ spec/injekt_spec.rb
@@ -4,8 +4,32 @@ describe Array do
 
   let(:array) {[1, 2, 3, 4, 5]}
 
-  it '.injekt produces the same result as .inject' do
-    expect(array.injekt(array) {|memo, i| memo - i}).to eq array.inject(:-)
+  context 'simple math' do
+
+    it 'addition' do
+      expect(array.injekt(array) {|memo, i| memo + i}).to eq array.inject(:+)
+    end
+
+    it 'subtraction' do
+      expect(array.injekt(array) {|memo, i| memo - i}).to eq array.inject(:-)
+    end
+
+    it 'multiplication' do
+      expect(array.injekt(array) {|memo, i| memo * i}).to eq array.inject(:*)
+    end
+
+    it 'division' do
+      expect(array.injekt(array) {|memo, i| memo/i}).to eq array.inject(:/)
+    end
+
+  end
+
+  context 'complex operations' do
+
+    it 'handles more complex operations' do
+      expect(array.injekt(array) {|memo, i| ((memo + 50)-(i^32))}).to eq array.inject {|memo, i| ((memo + 50)-(i^32))}
+    end
+
   end
 
 end
