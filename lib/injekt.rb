3f7aa85510ff10ed3009e2f58b63038d1977422b
Rufus Raghunath <rufusraghunath@gmail.com>
2016-01-31 21:18:44 +0000

    Added sending functionality, allows passing symbols as methods but currently prevents using blocks

--- lib/injekt.rb
+++ lib/injekt.rb
@@ -1,14 +1,14 @@
 class Array
 
-  def injekt memo=self[0]
+  def injekt memo=self[0], block
     if memo == self[0]
       self.each do |i|
         next if i == self[0]
-        memo = yield memo, i
+        memo = memo.send(block, i)
       end
     else
       self.each do |i|
-        memo = yield memo, i
+        memo = memo.send(block, i)
       end
     end
     memo
--- spec/injekt_spec.rb
+++ spec/injekt_spec.rb
@@ -7,19 +7,19 @@ describe Array do
   context 'simple math' do
 
     it 'addition' do
-      expect(array.injekt(&:+)).to eq array.inject(:+)
+      expect(array.injekt(:+)).to eq array.inject(:+)
     end
 
     it 'subtraction' do
-      expect(array.injekt(&:-)).to eq array.inject(:-)
+      expect(array.injekt(:-)).to eq array.inject(:-)
     end
 
     it 'multiplication' do
-      expect(array.injekt(&:*)).to eq array.inject(:*)
+      expect(array.injekt(:*)).to eq array.inject(:*)
     end
 
     it 'division' do
-      expect(array.injekt(&:/)).to eq array.inject(:/)
+      expect(array.injekt(:/)).to eq array.inject(:/)
     end
 
   end
@@ -27,14 +27,14 @@ describe Array do
   context 'complex operations' do
 
     it 'handles more complex operations' do
-      expect(array.injekt {|memo, i| ((memo + 50)-(i**32))/10}).to eq array.inject {|memo, i| ((memo + 50)-(i**32))/10}
+      expect(array.injekt (|memo, i| ((memo + 50)-(i**32))/10}).to eq array.inject {|memo, i| ((memo + 50)-(i**32))/10}
     end
 
   end
 
   context 'memo variability' do
     it 'it works for non-default values of memo' do
-      expect(array.injekt(4, &:+)).to eq array.inject(4, :+)
+      expect(array.injekt(4, :+)).to eq array.inject(4, :+)
     end
   end
 
